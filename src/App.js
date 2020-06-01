c31950cdfe8bb476879dc3f491d0eadd43974520
ayryu <ayryu@uwaterloo.ca>
2020-06-01 14:51:54 -0400

    Icons now have basic functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 import NavBar from './components/navbar';
 import Project from './components/project';
-import IconButton from '@material-ui/core/IconButton';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
@@ -92,39 +88,6 @@ function App() {
   return (
     <div className={classes.root}>
       <NavBar />
-      {/* <div className={classes.text}>
-        <Typography variant="h4" style= {{ fontWeight: 'bolder' }}>
-          Lifedisassembler
-        </Typography>
-      </div> */}
-
-      {/* <div className={classes.links}>
-        <Grid container className={classes.container} spacing={0}>
-          
-            <Grid item xs={6} sm={3}>
-
-              <IconButton>
-                <LinkedInIcon  className={classes.griditem}/>
-              </IconButton>
-            </Grid>
-
-            <Grid item className={classes.griditem} xs={6} sm={3}>
-              <IconButton>
-                <GitHubIcon />
-              </IconButton>
-            </Grid>
-          
-        </Grid> 
-      </div> */}
-
-        {/* <Grid container spacing={1}>  
-          {projects.map(project => 
-            <Grid item xs={12} >
-              <Project project={project}/>
-            </Grid>
-          )}
-        </Grid> */}
-
     </div>
   );
 }
