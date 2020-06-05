61fb31a7657e5e3928e1b823add662ba2e7c6887
ayryu <ayryu@uwaterloo.ca>
2020-06-05 18:16:23 -0400

    Added dark/light mode resume options and adjusted tab fonts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import NavBar from './components/navbar';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-
-const Copyright = () => {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-        Alexander Ryu {''}  
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles({
   root: {
@@ -26,7 +14,6 @@ function App() {
   return (
     <div className={classes.root}>
       <NavBar />
-      <Copyright style= {{ alignContent: 'flex-end' }}/>
     </div>
   );
 }
--- src/components/icons.js
+++ src/components/icons.js
@@ -7,22 +7,24 @@ import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import AssignmentIndOutlinedIcon from '@material-ui/icons/AssignmentIndOutlined';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import BeenhereIcon from '@material-ui/icons/Beenhere';
 import Collapse from '@material-ui/core/Collapse';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
+import Resume from './resume';
 
 const useStyles = makeStyles((theme) => ({
-    links: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      minWidth: 'auto'
+    button: {
+      width: 60,
+      height: 60,
     },
     collapse: {
       alignSelf: 'center',
       width: '80%',
       paddingTop: theme.spacing(2),
+      },
+    links: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minWidth: 'auto'
     },
     media: {
       // height: 0, paddingTop: '100%', paddingBottom: '45%' for perfect visibility if collapse width: '80%'
@@ -34,35 +36,30 @@ const useStyles = makeStyles((theme) => ({
 
 const Icons = () => {
     const classes = useStyles();
-    // const [ skillsPanel, setSkillsPanel] = useState(false);
     const [ resumePanel, setResumePanel] = useState(false);
-
-    // const handleSkillsChange = () => {
-    //     setSkillsPanel((prev) => !prev);
-    //     setResumePanel(false);
-    //   };
+    const [ resumeView, setResumeView ] = useState(false);
     
-      const handleResumeChange = () => {
+      const handleResumeOpen = () => {
         setResumePanel((prev) => !prev);
-        // setSkillsPanel(false);
       };
 
+      const toggleResumeView = (event) => {
+          event.preventDefault();
+          setResumeView(view => !view);
+      }
+
+      console.log('resumeView: ', resumeView)
+
     const collapseInfo = [
-        // {
-        //     in: skillsPanel,
-        //     image: '/static/images/pics/resume.png',
-        // },
         {
+            id: 1,
             in: resumePanel,
-            image: '/static/images/pics/resume.png',
+            image: resumeView
+            ?  '/static/images/pics/lightresume.png' : '/static/images/pics/darkresume.png',
         },
     ]
 
     const icons = [
-        // {
-        //     title: 'Skills',
-        //     id: 'SKILLS',
-        // },
         {
             title: 'Resume',
             id: 'RESUME',
@@ -83,28 +80,24 @@ const Icons = () => {
 
     const getIcon = (icon) => {
         switch(icon) {
-            // case 'SKILLS':
-            //     return(<BeenhereIcon 
-            //         fontSize='large'
-            //         onClick={handleSkillsChange} />);
             case 'RESUME':
                 return(<AssignmentIndOutlinedIcon 
-                    fontSize='large'
-                    onClick={handleResumeChange} />); 
+                    style={{width: 60, height: 60,}} 
+                    onClick={handleResumeOpen} />); 
             case 'EMAIL':
                 return(<MailOutlineIcon 
-                    fontSize='large' 
+                    style={{width: 60, height: 60,}} 
                     aria-label='Email'
                     onClick={event => window.location.href='mailto:ayryu@uwaterloo.ca'}/>);      
             case 'GITHUB':
                 return(<GitHubIcon 
-                    fontSize='large' 
+                    style={{width: 60, height: 60,}} 
                     aria-label='Github'
                     onClick={event => window.location.href='https://github.com/ayryu'}
                     />);                  
             default:
                 return(<LinkedInIcon 
-                    fontSize='large'
+                    style={{width: 60, height: 60,}} 
                     aria-label='LinkedIn' 
                     onClick={event => window.location.href='https://www.linkedin.com/in/alexander-ryu/'}/>);
         }
@@ -113,19 +106,20 @@ const Icons = () => {
     return (
         <>
             {collapseInfo.map(element => 
-                <Collapse className={classes.collapse} in={element.in} timeout='auto' unmountOnExit>
-                    <Card>
-                        <CardMedia 
-                            className={classes.media}
-                            image={element.image}
-                        />
-                    </Card>
+                <Collapse key={element.id} className={classes.collapse} in={element.in} timeout='auto' unmountOnExit>
+                    <Resume 
+                        resume={element}
+                        media={classes.media}
+                        viewState={resumeView} 
+                        toggleView={toggleResumeView}
+                        handleCollapse={handleResumeOpen}
+                    />
                 </Collapse>
             )}
 
             <Grid container className={classes.links} background="#000" spacing={0}>
                 {icons.map(icon =>
-                    <Grid item>
+                    <Grid item key={icon.id}>
                         <IconButton>
                             <Tooltip title={icon.title} arrow>
                                 {getIcon(icon.id)}
